eaacb52ccf43990629d45cb7d1742e0fafa6c628
Muhammad Moeed <134531518+Muhammad-Moeed@users.noreply.github.com>
2025-01-25 16:35:29 +0500

    Update app.js del priew func
    delete priew func

--- app.js
+++ app.js
@@ -4,17 +4,3 @@ function toggleFavorite(productId) {
     favIcon.classList.toggle("text-secondary");
   }
   
-  function previewImage(event) {
-    const file = event.target.files[0]; 
-    const reader = new FileReader();
-  
-    reader.onload = function(e) {
-      const productImage = document.getElementById('productImage');
-      productImage.src = e.target.result;  
-    };
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-  }
-  
